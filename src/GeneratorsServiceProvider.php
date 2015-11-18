59f6d2463a3dbeecc6fb9f13dec5aacd4c70fc5b
Dean Tedesco <dino@tedesco.net.au>
2015-11-19 10:55:32 +1100

    Added all Laravel Make commands to generators for use in Lumen.

--- src/GeneratorsServiceProvider.php
+++ src/GeneratorsServiceProvider.php
@@ -19,10 +19,122 @@ class GeneratorsServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->registerCommandGenerator();
+        $this->registerConsoleGenerator();
+        $this->registerEventGenerator();
+        $this->registerJobGenerator();
+        $this->registerListenerGenerator();
+        $this->registerModelGenerator();
+        $this->registerProviderGenerator();
         $this->registerRepositoryGenerator();
+        $this->registerTestGenerator();
         $this->registerTransformerGenerator();
     }
 
+    /**
+     * Register the make:command generator.
+     */
+    private function registerCommandGenerator()
+    {
+        $this->app->singleton(
+            'command.thirdsense.Command',
+            function ($app) {
+                return $app[CommandMakeCommand::class];
+            }
+        );
+
+        $this->commands('command.thirdsense.Command');
+    }
+
+    /**
+     * Register the make:console generator.
+     */
+    private function registerConsoleGenerator()
+    {
+        $this->app->singleton(
+            'command.thirdsense.console',
+            function ($app) {
+                return $app[ConsoleMakeCommand::class];
+            }
+        );
+
+        $this->commands('command.thirdsense.console');
+    }
+
+    /**
+     * Register the make:event generator.
+     */
+    private function registerEventGenerator()
+    {
+        $this->app->singleton(
+            'command.thirdsense.event',
+            function ($app) {
+                return $app[EventMakeCommand::class];
+            }
+        );
+
+        $this->commands('command.thirdsense.event');
+    }
+
+    /**
+     * Register the make:job generator.
+     */
+    private function registerJobGenerator()
+    {
+        $this->app->singleton(
+            'command.thirdsense.job',
+            function ($app) {
+                return $app[JobMakeCommand::class];
+            }
+        );
+
+        $this->commands('command.thirdsense.job');
+    }
+
+    /**
+     * Register the make:listener generator.
+     */
+    private function registerListenerGenerator()
+    {
+        $this->app->singleton(
+            'command.thirdsense.listener',
+            function ($app) {
+                return $app[ListenerMakeCommand::class];
+            }
+        );
+
+        $this->commands('command.thirdsense.listener');
+    }
+
+    /**
+     * Register the make:model generator.
+     */
+    private function registerModelGenerator()
+    {
+        $this->app->singleton(
+            'command.thirdsense.model',
+            function ($app) {
+                return $app[ModelMakeCommand::class];
+            }
+        );
+
+        $this->commands('command.thirdsense.model');
+    }
+
+    /**
+     * Register the make:provider generator.
+     */
+    private function registerProviderGenerator()
+    {
+        $this->app->singleton(
+            'command.thirdsense.provider',
+            function ($app) {
+                return $app[ProviderMakeCommand::class];
+            }
+        );
+
+        $this->commands('command.thirdsense.provider');
+    }
 
     /**
      * Register the make:repository generator.
@@ -37,6 +149,21 @@ class GeneratorsServiceProvider extends ServiceProvider
         );
 
         $this->commands('command.thirdsense.repository');
+    }
+
+    /**
+     * Register the make:test generator.
+     */
+    private function registerTestGenerator()
+    {
+        $this->app->singleton(
+            'command.thirdsense.test',
+            function ($app) {
+                return $app[TestMakeCommand::class];
+            }
+        );
+
+        $this->commands('command.thirdsense.test');
     }
 
     /**
